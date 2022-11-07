79392cb709274726db248f47445e1303ea321a15
Kevin Martinez <kkevinmartinez7@gmail.com>
2022-11-07 11:30:16 -0600

    Sent message in realtime

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -52,6 +52,8 @@
 @push('scripts')
 <script type="module">
     const usersElement = document.getElementById('users');
+    const messagesElement = document.getElementById('messages');
+
     Echo.join('chat')
         .here((users) => {
             users.forEach((user, index) => {
@@ -78,6 +80,14 @@
             let element = document.getElementById(user.id);
             element.parentNode.removeChild(element)
         })
+        .listen('MessageSent', (e) => {
+            let element = document.createElement('li')
+
+            element.setAttribute('id', e.user.id);
+            element.innerText = e.user.name + ': ' + e.message;
+
+            messagesElement.appendChild(element);
+        });
 </script>
 
 <script style="module">
